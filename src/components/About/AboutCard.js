a33c37c62831f519df60e6918123cb18756fb746
Recker4244 <ashutoshsenapati2311@gmail.com>
2023-06-28 01:00:30 +0530

    fix: unused import warning

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
-import { ImPointRight } from "react-icons/im";
 
 function AboutCard() {
   return (
@@ -10,30 +9,22 @@ function AboutCard() {
           <p style={{ textAlign: "justify" }}>
             Hi Everyone, I am <span className="purple">Ashutosh Senapati </span>
             from <span className="purple"> Bhubaneswar, India.</span>
-            <br /> I am a final year student pursuing a Bachelor's in Technology (B.Tech)
-            in Computer Science and Engineering at KIIT University.
+            <br /> I am a final year student pursuing a Bachelor's in Technology
+            (B.Tech) in Computer Science and Engineering at KIIT University.
             <br />
-            Additionally, I am currently employed as a Software Engineer Intern at
-            McKinsey & Company.
+            Additionally, I am currently employed as a Software Engineer Intern
+            at McKinsey & Company.
             <br />
             <br />
             Apart from coding, some other activities that I love to do!
           </p>
           <ul>
-            <li className="about-activity">
-            Playing Games 🎮
-            </li>
-            <li className="about-activity">
-              Reading 📖
-            </li>
-            <li className="about-activity">
-              Travelling ✈️
-            </li>
+            <li className="about-activity">Playing Games 🎮</li>
+            <li className="about-activity">Reading 📖</li>
+            <li className="about-activity">Travelling ✈️</li>
           </ul>
 
-          <p style={{ color: "rgb(155 126 172)" }}>
-            "Never Settle"{" "}
-          </p>
+          <p style={{ color: "rgb(155 126 172)" }}>"Never Settle" </p>
         </blockquote>
       </Card.Body>
     </Card>
